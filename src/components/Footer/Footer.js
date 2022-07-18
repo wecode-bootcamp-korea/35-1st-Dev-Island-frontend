eb0a3f4b034dafdc73568d41366b77115dcc4c3c
hoonjo1 <hoonjo1@gmail.com>
2022-07-19 00:35:07 +0900

    add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Footer.scss';
+
+const Footer = () => {
+  return <footer className="footer-container" />;
+};
+
+export default Footer;
