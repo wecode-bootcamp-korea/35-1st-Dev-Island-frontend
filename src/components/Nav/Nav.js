6b1cafbe245d3e35a6f514d91bca7ea4d5b215dc
hoonjo1 <hoonjo1@gmail.com>
2022-07-20 01:36:39 +0900

    img 경로 수정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
     <nav className="nav-container">
       <div className="nav-container__left">
         <Link to="/">
-          <img alt="logo" src="images/W_O_Black.svg" />
+          <img alt="logo" src="/images/Nav/W_O_Black.svg" />
         </Link>
         <div>
           <Link to="/productlist">
@@ -23,15 +23,15 @@ const Nav = () => {
       <div className="nav-container__right">
         <span>
           <Link to="/signin">
-            <img alt="logo" src="images/user_icon.svg" />
+            <img alt="logo" src="/images/Nav/user.svg" />
           </Link>
         </span>
         <span>
-          <img alt="logo" src="images/location_icon.svg" />
+          <img alt="logo" src="/images/Nav/location.svg" />
         </span>
         <span>
           <Link to="/cart">
-            <img alt="logo" src="images/cart_icon.svg" />
+            <img alt="logo" src="/images/Nav/cart.svg" />
           </Link>
         </span>
       </div>
