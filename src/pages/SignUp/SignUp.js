1a9e63baa64060f5a05bec26e4a435aca137a16d
hoonjo1 <hoonjo1@gmail.com>
2022-07-20 01:31:29 +0900

    sign-up 구현 중

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,20 +1,82 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './SignUp.scss';
 
 function SignUp() {
+  const [values, setValues] = useState({
+    frist_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    cofirm_password: '',
+  });
+
+  const handleInput = e => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const emailRegExp =
+    /^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+  const passwordRegExp =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
+
+  const cofirmEmail =
+    values.email.length > 0 && !emailRegExp.test(values.email);
+
+  const cofirmPassword =
+    values.cofirm_password && values.password !== values.cofirm_password;
+
+  const test = passwordRegExp.test(values.password);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(test);
+  };
+
   return (
     <main className="sign-in-container">
       <div className="sign-in-inner">
         <div className="inner-left">
           <h1>내 계정</h1>
-          <form>
-            <input type="text" placeholder="Frist Name *" />
-            <input type="text" placeholder="Last Name *" />
-            <input type="email" placeholder="Email *" />
-            <input type="password" placeholder="Password *" />
-            <input type="password" placeholder="Confirm Password *" />
+          <form onSubmit={onSubmit}>
+            <input
+              onChange={handleInput}
+              name="frist_name"
+              type="text"
+              placeholder="Frist Name *"
+            />
+            <input
+              onChange={handleInput}
+              name="last_name"
+              type="text"
+              placeholder="Last Name *"
+            />
+            {cofirmEmail ? (
+              <label className="input-label">이메일 형식을 확인하세요.</label>
+            ) : null}
+            <input
+              onChange={handleInput}
+              name="email"
+              type="email"
+              placeholder="Email *"
+            />
+            {cofirmPassword ? (
+              <label className="input-label">비밀번호를 확인하세요.</label>
+            ) : null}
+            <input
+              onChange={handleInput}
+              name="password"
+              type="password"
+              placeholder="Password *"
+            />
+            <input
+              onChange={handleInput}
+              name="cofirm_password"
+              type="password"
+              placeholder="Confirm Password *"
+            />
             <p>
               이 사이트는 보호되며 We & Olufsen 개인 정보 취급 정책 및 서비스
               약관이 적용됩니다.
