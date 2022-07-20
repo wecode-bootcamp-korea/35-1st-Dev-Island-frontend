a5a943019f9882562d39e72f86e91a81bf51b397
hoonjo1 <hoonjo1@gmail.com>
2022-07-20 16:45:00 +0900

    ADD config

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'http://10.58.6.177:8000';
+
+const API = {
+  signin: `${BASE_URL}/users/login`,
+  signup: `${BASE_URL}/users/signup`,
+  product: `${BASE_URL}/product`,
+  productDetail: `${BASE_URL}/product/detail/`,
+  cart: `${BASE_URL}/cart`,
+  order: `${BASE_URL}/order`,
+};
+
+export default API;
