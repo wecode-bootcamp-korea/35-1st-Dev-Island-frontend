725512b01996b9cf333c6cbe3694955deec34769
Hyunjoo Lee <hyunjoo0572@gmail.com>
2022-07-26 17:50:37 +0900

    수정

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,21 +2,21 @@ import React from 'react';
 import './Modal.scss';
 
 const Modal = props => {
-  const { modal, open, close, header, children } = props;
+  const { modalRef, modalOpen, modalClose, header, children, moveItem } = props;
 
   return (
-    <div className={open ? 'openModal modal' : 'modal'} ref={modal}>
-      {open && (
+    <div className={modalOpen ? 'openModal modal' : 'modal'} ref={modalRef}>
+      {modalOpen && (
         <section>
           <header>
             {header}
-            <button className="close" onClick={close}>
+            <button className="close" onClick={modalClose}>
               &times;
             </button>
           </header>
           <main>{children}</main>
           <footer>
-            <button className="gotocart" onClick={close}>
+            <button className="gotocart" onClick={moveItem}>
               장바구니로 이동
             </button>
           </footer>
--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,61 +1,48 @@
 import React, { useEffect, useState, useRef } from 'react';
-// import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import Modal from '../../components/Modal/Modal';
 import ProductDetailContents from '../../components/ProductDetailContents/ProductDetailContents';
 import './ProductDetail.scss';
 
 function ProductDetail() {
-  const [modalOpen, setModalOpen] = useState(false);
-  const token = localStorage.getItem('login-token');
-
-  const openModal = () => {
-    token ? setModalOpen(true) : navigate('/signin');
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-  useEffect(() => {
-    const escKeyModalClose = e => {
-      if (e.keyCode === 27) {
-        setModalOpen(false);
-      }
-    };
-    window.addEventListener('keydown', escKeyModalClose);
-    window.addEventListener('click', ClickOutsideHandler);
-    return () => {
-      window.removeEventListener('keydown', escKeyModalClose);
-      window.removeEventListener('click', ClickOutsideHandler);
-    };
-  }, []);
-
-  function ClickOutsideHandler(event) {
-    if (modal.current === event.target) {
-      setModalOpen(false);
-    }
-  }
-
+  const [isOpenModal, setIsOpenModal] = useState(false);
   const [product, setProduct] = useState({});
+  const outlineRef = useRef(null);
+  const featuresRef = useRef(null);
+  const modalRef = useRef(null);
+  const { product_image, price, name, content_url, description } = product;
+  const { id } = useParams();
 
-  const ref1 = useRef(null);
-  const ref2 = useRef(null);
-  const modal = useRef(null);
-
-  const scrollToElement = () => {
-    ref1.current.scrollIntoView();
+  const openPurchaseModal = () => {
+    const token = sessionStorage.getItem('login-token');
+    token ? setIsOpenModal(true) : navigate('/signin');
   };
 
-  const handleButton = () => {
-    ref2.current.scrollIntoView();
+  const isCloseModal = () => {
+    setIsOpenModal(false);
   };
-  // const params = useParams();
-  useEffect(() => {
-    fetch('http://10.58.1.160:8000/products/16')
-      .then(res => res.json())
-      .then(data => setProduct(data.result));
-  }, []);
 
+  const moveItem = async () => {
+    const url = 'http://10.58.4.137:8000/carts';
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization:
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NH0.bd9JCUK-PC6dAZc4WyRjjEw6zwaqw2YtsaANRY6YKjo',
+      },
+      body: JSON.stringify({
+        product_id: `${id}`,
+        quantity: 1,
+      }),
+    });
+    const result = await response.json();
+    if (result.message === 'PUT_IN_CART_SUCCESS' || 'CART_QUANTITY_CHANGED') {
+      navigate('/cart');
+    } else {
+      alert('에러입니다.');
+    }
+  };
   const navigate = useNavigate();
   const goToProductList = () => {
     navigate('/products');
@@ -64,12 +51,47 @@ function ProductDetail() {
     navigate('/cart');
   };
 
+  const escKeyModalClose = e => {
+    if (e.keyCode === 27) {
+      setIsOpenModal(false);
+    }
+  };
+
+  const clickOutsideHandler = event => {
+    if (modalRef.current === event.target) {
+      setIsOpenModal(false);
+    }
+  };
+  const scrollToElement = () => {
+    outlineRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const handleButton = () => {
+    featuresRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', escKeyModalClose);
+    window.addEventListener('click', clickOutsideHandler);
+
+    return () => {
+      window.removeEventListener('keydown', escKeyModalClose);
+      window.removeEventListener('click', clickOutsideHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    fetch(`http://10.58.4.137:8000/products/${id}`)
+      .then(res => res.json())
+      .then(data => setProduct(data.result));
+  }, []);
+
   return (
     <div className="product-detail">
-      <div className="product-detail-wrap" ref={ref1}>
+      <div className="product-detail-wrap" ref={outlineRef}>
         <div className="product-detail-wrap-left">
           <div className="product-detail-wrap-left__photo">
-            <img src={product.product_image} alt="product" />
+            <img src={product_image} alt="product" />
 
             <button type="button" onClick={goToProductList}>
               <img src="/images/ProductDetail/left-arrow.png" alt="arrow" />
@@ -79,25 +101,23 @@ function ProductDetail() {
         </div>
 
         <Modal
-          modal={modal}
-          open={modalOpen}
-          close={closeModal}
+          modalRef={modalRef}
+          modalOpen={isOpenModal}
+          modalClose={isCloseModal}
           header="상품이 장바구니에 추가됨"
           product={product}
           goToCart={goToCart}
+          moveItem={moveItem}
         >
           <div className="modal-wrap">
             <div className="modal-wrap-top">
-              <img src={product.product_image} alt="product" />
+              <img src={product_image} alt="product" />
               <div className="modal-wrap-top-name">
-                <p>{product.name}</p>
+                <p>{name}</p>
                 <p>Natural Oak</p>
               </div>
               <div className="modal-wrap-top-price">
-                ₩
-                {parseInt(product.price)
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                ₩{parseInt(price).toLocaleString()}
               </div>
             </div>
             <div className="modal-wrap-bottom">
@@ -121,26 +141,21 @@ function ProductDetail() {
         </Modal>
 
         <div className="product-detail-wrap-right">
-          <div className="product-detail-wrap-right__name">{product.name}</div>
+          <div className="product-detail-wrap-right__name">{name}</div>
           <div className="product-detail-wrap-right__short-desc">
-            <h2>{product.description}</h2>
+            <h2>{description}</h2>
           </div>
           <div className="product-detail-wrap-right__long-desc">
             {`언제나 나와 함께하는 레벨. 언제 어디서나 성능, 휴대성, 연결성을 제공합니다. 
             세워서, 또는 눕혀서, 내가 좋아하는 노래를 항상 내곁에.`}
           </div>
           <div className="product-detail-wrap-right__price">
-            <h4>
-              ₩
-              {parseInt(product.price)
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-            </h4>
+            <h4>₩{parseInt(price).toLocaleString()}</h4>
           </div>
-          <button onClick={openModal} className="buttontop">
+          <button onClick={openPurchaseModal} className="purchase-button">
             구매 하기
           </button>
-          <button className="buttonbottom">매장에서 체험하기</button>
+          <button className="experience-button">매장에서 체험하기</button>
         </div>
       </div>
 
@@ -152,9 +167,9 @@ function ProductDetail() {
         <li>리뷰</li>
       </ul>
 
-      <div className="product-detail-container" ref={ref2}>
+      <div className="product-detail-container" ref={featuresRef}>
         <div className="product-detail-container__main-image">
-          <img src={product.content_url} alt="mainimage" />
+          <img src={content_url} alt="mainimage" />
         </div>
         <div className="product-detail-container__keyword">
           <p>최고의 휴대성</p>
