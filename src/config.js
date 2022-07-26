539d0b5fc9cd4170e66caf0c1582a6efbb6b7c21
hoonjo1 <hoonjo1@gmail.com>
2022-07-26 12:12:21 +0900

    ing

--- src/config.js
+++ src/config.js
@@ -3,9 +3,10 @@ const BASE_URL = 'http://10.58.6.177:8000';
 const API = {
   signin: `${BASE_URL}/users/login`,
   signup: `${BASE_URL}/users/signup`,
-  product: `${BASE_URL}/product`,
-  productDetail: `${BASE_URL}/product/detail/`,
-  cart: `${BASE_URL}/cart`,
+  product: `${BASE_URL}/products`,
+  productDetail: `${BASE_URL}/products/`,
+  cart: `${BASE_URL}/carts`,
+  order: `${BASE_URL}/myorder`,
 };
 
 export default API;
